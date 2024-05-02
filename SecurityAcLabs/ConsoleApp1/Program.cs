c13ee7753a80433d84a61a0a87f10fc71964263c
breixs <paulbrei25@gmail.com>
2024-05-02 15:25:47 +0300

    Added PasswordAPI

--- SecurityAcLabs/ConsoleApp1/Program.cs
+++ SecurityAcLabs/ConsoleApp1/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MyAPICode;
+
+var api = new PasswordAPI();
+var result = api.GetPasswordAPI(10);
+string[] results = result.Split('"');
+Console.WriteLine(result);
+Console.WriteLine(results[3]);
+
---
+++ SecurityAcLabs/MyAPICode/PasswordAPI.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+using RestSharp.Authenticators;
+
+namespace MyAPICode
+{
+    public class PasswordAPI
+    {
+        //private string apiURL="url";
+        private HttpClient _client;
+        public PasswordAPI()
+        {
+            _client = new HttpClient();
+        }
+
+        public string GetPasswordAPI(int length)
+        {
+            var response = _client.GetAsync($"https://api.genratr.com/?length={length}&uppercase&lowercase&special&numbers").Result;
+            var result = response.Content.ReadAsStringAsync().Result;
+            return result;
+        }
+    }
+}
