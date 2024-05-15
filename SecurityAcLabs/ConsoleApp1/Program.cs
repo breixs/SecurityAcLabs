9ee1b3a55e96cc74a180a7e4a15aaefa22c9c7fd
Brisc Paul <alex_brisc@yahoo.com>
2024-05-15 16:56:16 +0300

    Hidden Api Key, URLScan analysis response

--- SecurityAcLabs/ConsoleApp1/Program.cs
+++ SecurityAcLabs/ConsoleApp1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using MyAPICode;
 using VirusTotal;
+using System.IO;
 
 var api = new PasswordAPI();
 var result = api.GetPasswordAPI(16);
@@ -10,3 +11,9 @@ Console.WriteLine(results[3]);
 var api2 = new URLScan();
 var result2 = await api2.GetURL();
 Console.WriteLine(result2);
+
+string filePath = "analysis.txt";
+var apiAnalysis= new URLScanAnalysis();
+var analysis = await apiAnalysis.GetAnalysis(result2.Data.Links.Self);
+string json = Newtonsoft.Json.JsonConvert.SerializeObject(analysis, Newtonsoft.Json.Formatting.Indented);
+Console.WriteLine(json);
--- SecurityAcLabs/VirusTotal/UrlResponse.cs
+++ SecurityAcLabs/VirusTotal/UrlResponse.cs
@@ -6,27 +6,96 @@ using System.Threading.Tasks;
 
 namespace VirusTotal
 {
-        public class UrlResponse
-        {
-            public Data Data { get; set; }
+    public class UrlResponse
+    {
+        public Data Data { get; set; }
 
         public override string ToString()
         {
-            return $"Type: {Data.Type}\n" +
-                $"Id: {Data.Id}\n" +
-                $"Self: {Data.Links.Self}";
-        }
-    }
-    
-    public class Data
-        {
-            public string Type { get; set; }
-            public string Id { get; set; }
-            public Links Links { get; set; }
+            var sb = new StringBuilder();
+            sb.AppendLine($"Type: {Data?.Type}");
+            sb.AppendLine($"Id: {Data?.Id}");
+            sb.AppendLine($"Self: {Data?.Links?.Self}");
+            if(Data?.Attributes != null)
+            {
+                sb.AppendLine($"Status: {Data?.Attributes?.Status}");
+                
+                sb.AppendLine($"Date: {Data?.Attributes?.Date}");
+                sb.AppendLine($"Malicious: {Data?.Attributes?.Stats?.Malicious}");
+                sb.AppendLine($"Suspicious: {Data?.Attributes?.Stats?.Suspicious}");
+                sb.AppendLine($"Undetected: {Data?.Attributes?.Stats?.Undetected}");
+                sb.AppendLine($"Harmless: {Data?.Attributes?.Stats?.Harmless}");
+                sb.AppendLine($"Timeout: {Data?.Attributes?.Stats?.Timeout}");
+            }
+            
+            
+            return sb.ToString();
         }
 
-        public class Links
+    }
+
+    public class Data
+    {
+        public string Type { get; set; }
+        public string Id { get; set; }
+        public Links Links { get; set; }
+        public Attributes Attributes { get; set; }
+    }
+
+    public class Links
+    {
+        public string Self { get; set; }
+        public string Item { get; set; }
+    }
+
+    public class Attributes
+    {
+        public Dictionary<string, EngineResult> Results { get; set; }
+        public long Date { get; set; }
+        public string Status { get; set; }
+        public Stats Stats { get; set; }
+    }
+
+    public class EngineResult
+    {
+        public string Method { get; set; }
+        public string Engine_Name { get; set; }
+        public string Category { get; set; }
+        public string Result { get; set; }
+
+        public EngineResult(string method, string engineName, string category, string result)
         {
-            public string Self { get; set; }
+            Method = method;
+            Engine_Name = engineName;
+            Category = category;
+            Result = result;
         }
+    }
+
+    public class Stats
+    {
+        public int Malicious { get; set; }
+        public int Suspicious { get; set; }
+        public int Undetected { get; set; }
+        public int Harmless { get; set; }
+        public int Timeout { get; set; }
+    }
+
+    public class Meta
+    {
+        public UrlInfo UrlInfo { get; set; }
+        public FileInfo FileInfo { get; set; }
+    }
+
+    public class UrlInfo
+    {
+        public string Id { get; set; }
+        public string Url { get; set; }
+    }
+
+    public class FileInfo
+    {
+        public string Sha256 { get; set; }
+    }
+
 }
